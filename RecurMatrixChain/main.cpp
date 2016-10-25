c29ed064a65eb83022d8b4685cfb9849f76a451f
lemonnation <sjtuxiaohan@sjtu.edu.cn>
2016-10-25 15:01:02 +0800

    lala

--- RecurMatrixChain/main.cpp
+++ RecurMatrixChain/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
 
 using namespace std;
 
 int main() {
     //std::cout << "Hello, World!" << std::endl;
+    /*string str;
+    int a, b;
+    int c;
+    while(cin>>str)
+    {
+        cin>>a>>b;
+        c = a+ b;
+        cout<<str<<":"<<c<<endl;
+
+
+    }*/
+    /*
+    vector<int> vec;
+    int i = 0;
+    for(;i < 5; i ++)
+    {
+        vec.push_back(i*i);
+    }
+    vector<int>::iterator myIter  = vec.begin();
+    while(myIter != vec.end())
+    {
+        cout<<"hello is :"<<*myIter<<endl;
+        myIter++;
+    }*/
 
     return 0;
 }
